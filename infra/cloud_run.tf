6f102d165f9f92e94daa06641b7e64c483fb3495
kylej21 <kylecj21@gmail.com>
2026-04-24 23:42:36 -0700

    fix attempt 2

--- infra/cloud_run.tf
+++ infra/cloud_run.tf
@@ -83,10 +83,7 @@ resource "google_cloud_run_v2_service" "proxy" {
   }
 
   lifecycle {
-    ignore_changes = [
-      template[0].containers[0].image,
-      template[0].containers[0].env,
-    ]
+    ignore_changes = [template[0].containers[0].image]
   }
 
   depends_on = [
